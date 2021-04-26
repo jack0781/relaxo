44e04e2de51c1f8e4e2ef9f7ac176e4f40b35bb9
jack0781 <jaykatakiya8946@gmail.com>
2021-04-26 20:25:35 +0530

    first commit

---
+++ src/apis/youtubeSearch.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+// put your api keys in .env file you can get those here - https://developers.google.com/youtube/v3/getting-started
+export const selectRandomKey = () => {
+  const api = "AIzaSyD30rojpPR8nCxbG12t97FDjxPsUKTbCvI";
+  const keys = api.split(" "); //we are splitting the api keys to make an array
+  const random = Math.floor(Math.random() * Math.floor(keys.length)); //this will get a random number
+  return keys[random];
+};
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    videoCategoryId: "10",
+    type: "video",
+    key: selectRandomKey(),
+  },
+});
